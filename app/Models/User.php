51a59805cf8ee10d237f7dfe11c2e3fe3bc63361
wencai159@126.com <wencai159@126.com>
2020-11-20 13:22:47 +0800

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','introduction','avatar',
     ];
 
     /**
