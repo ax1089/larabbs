ac2f5694f25acbaf27cd9139d7d35a151720d63e
wencai159@126.com <wencai159@126.com>
2020-12-15 01:14:05 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -10,13 +11,9 @@ use App\Models\Reply;
 class ReplyObserver
 {
  public function created(Reply $reply){
-//     $reply->topic->increment('reply_count',1);
      $reply->topic->reply_count = $reply->topic->replies->count();
      $reply->topic->save();
+     $reply->topic->user->notify(new TopicReplied($reply));
  }
 
-    public function creating(Reply $reply)
-    {
-        $reply->content = clean($reply->content, 'user_topic_body');
-    }
 }
